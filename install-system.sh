37dc6607df55496c768aca0cb7531eafef0d4309
Alexis Wurth <alexis.wurth57@gmail.com>
2017-03-24 16:33:00 +0100

    Added system installation script

---
+++ install-system.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+
+# Unity Tweak Tool
+apt install -y unity-tweak-tool
+
+# Numix Icon Theme Circle
+add-apt-repository -y ppa:numix/ppa &&
+    apt update &&
+    apt install -y numix-icon-theme-circle
+
+# Git
+apt install -y git
+
+# Google Chrome
+sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list' &&
+    apt update &&
+    apt install -y google-chrome-stable
+
+# CURL, HTTPie
+apt install -y curl httpie
+
+# cairo-dock, php, apache, mariadb, zsh, oh-my-zsh, docker, dropbox, gitkrakren, composer, nodejs, sass, arc-theme
+
+
+# Node: vuejs, bower, gulp, electron
