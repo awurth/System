a1482e84e4d2b2a35ab949b9f30ce57b1588f923
Alexis Wurth <alexis.wurth57@gmail.com>
2017-03-25 13:58:05 +0100

    Updated system install script

--- install-system.sh
+++ install-system.sh
@@ -2,8 +2,8 @@
 
 apt update && apt upgrade -y
 
-# Unity Tweak Tool
-apt install -y unity-tweak-tool
+# Unity Tweak Tool, Gimp
+apt install -y unity-tweak-tool gimp
 
 # Numix Icon Theme Circle
 add-apt-repository -y ppa:numix/ppa &&
@@ -22,9 +22,15 @@ sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt
 apt install -y curl httpie
 
 # Apache, PHP, MariaDB
-apt install -y apache2 php mariadb-server libapache2-mod-php php-mysql
+apt install -y apache2 php mariadb-server libapache2-mod-php php-mysql phpmyadmin
 
-# cairo-dock, php, apache, mariadb, zsh, oh-my-zsh, docker, dropbox, gitkrakren, composer, nodejs, sass, arc-theme
+# Zsh
+apt install zsh
+# Oh-my-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Set Zsh as default shell
+chsh -s $(which zsh)
 
+# cairo-dock, docker, dropbox, gitkrakren, composer, nodejs, sass, arc-theme, diff-so-fancy, virtualhost, atom
 
 # Node: vuejs, bower, gulp, electron
