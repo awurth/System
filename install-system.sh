8921e578304d378a421c47db210c11c704880022
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-04-28 19:34:41 +0200

    Update install script

--- install-system.sh
+++ install-system.sh
@@ -3,13 +3,9 @@
 apt update && apt upgrade -y
 
 # Themes
-add-apt-repository -y ppa:numix/ppa
-add-apt-repository -y ppa:noobslab/themes
-apt update
-apt install -y numix-icon-theme-circle arc-theme
 
-# Git, CURL, HTTPie
-apt install -y git curl httpie
+# Git, CURL
+apt install -y git curl
 
 # Apache, PHP, MySQL
 apt install -y apache2
@@ -19,8 +15,6 @@ apt install -y libapache2-mod-php
 apt install -y php-mysql
 
 # Node.js
-curl -sL https://deb.nodesource.com/setup_7.x | bash -
-apt install -y nodejs
 
 # Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -29,12 +23,7 @@ php -r "unlink('composer-setup.php');"
 mv composer /usr/bin/composer
 
 # Diff-so-fancy
-npm install -g diff-so-fancy bower gulp-cli vue-cli
-
-# f.lux
-add-apt-repository -y ppa:nathan-renniewaldock/flux
-apt update
-apt install -y fluxgui
+npm install -g diff-so-fancy
 
 # Docker
 apt install -y apt-transport-https ca-certificates software-properties-common
@@ -47,24 +36,8 @@ groupadd docker
 usermod -aG docker $USER
 systemctl enable docker
 
-# Unity Tweak Tool, Gimp
-apt install -y unity-tweak-tool cairo-dock nautilus-dropbox nautilus-image-converter gcolor2 gimp filezilla
-
-# Spotify
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-apt update
-apt install spotify-client
-
-# GitKraken
-curl -L https://release.gitkraken.com/linux/gitkraken-amd64.deb -o gitkraken-amd64.deb
-dpkg -i gitkraken-amd64.deb
-rm gitkraken-amd64.deb
-
-# Atom
-curl -L https://atom.io/download/deb -o atom-amd64.deb
-dpkg -i atom-amd64.deb
-rm atom-amd64.deb
+# Tools
+apt install -y gnome-tweak-tool gimp filezilla
 
 # Zsh
 apt install -y zsh
@@ -78,14 +51,10 @@ echo '
 # ---------- TODO ---------- #
 ##############################
 
-* Install powerline font
-* Set shell powerline font
-* Select Arc-Theme Darker and Numix Circle icons in unity-tweak-tool
 * Copy .zshrc, .gitconfig, .gitignore
 * Set Zsh as default shell: chsh -s $(which zsh)
-* Run cairo-dock on boot
 * Install
-    * Vivaldi, Google Chrome
+    * Google Chrome
     * PHPStorm, WebStorm
     * Discord
     * PHPMyAdmin
